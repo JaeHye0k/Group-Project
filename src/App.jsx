02f8a60872623d722dc69a3030a72e71628cd18f
JaeHye0k <ahhpc2012@naver.com>
2024-04-16 16:37:54 +0900

    레이아웃 페이지 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+import HomePage from "./pages/HomePage/HomePage";
+import AppLayout from "./layout/AppLayout/AppLayout";
+import TravelMap from "./pages/TravelMap/TravelMap";
+
+// 홈페이지 = /
+// 여행 지도 페이지 = /map
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<AppLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/map" element={<TravelMap />} />
+      </Route>
     </Routes>
   );
 }
---
+++ src/pages/TravelMap/TravelMap.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import KakaoMap from "../../common/KakaoMap";
+
+const TravelMap = () => {
+  return (
+    <div>
+      <KakaoMap />
+    </div>
+  );
+};
+
+export default TravelMap;
