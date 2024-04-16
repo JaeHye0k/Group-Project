12dae2006c5f9c9294a19ee8edccb0ae03c5779e
JaeHye0k <ahhpc2012@naver.com>
2024-04-16 15:31:52 +0900

    kakaoMap 추가

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import KakaoMap from "../common/KakaoMap";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <KakaoMap />
+    </div>
+  );
 };
 
 export default HomePage;
