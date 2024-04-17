8f787a2e4e28616ef3615f1892e8908089fb335e
JaeHyo0k <ahhpc2012@gmail.com>
2024-04-17 16:41:00 +0900

    리펙토링

--- src/pages/TravelMapPage/component/KakaoMap/KakaoMap.jsx
+++ src/pages/TravelMapPage/component/KakaoMap/KakaoMap.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./KakaoMap.style.css";
 import { getCurrentMapArea } from "../../../../utils/getCurrentMapArea";
+import { getCurrentLocaition } from "../../../../utils/getCurrentLocation";
 import { useSelector } from "react-redux";
 
 const { kakao } = window;
@@ -8,18 +9,9 @@ const baseUrl = `https://dapi.kakao.com/v2/local`;
 
 const KakaoMap = () => {
   const selectedCode = useSelector((state) => state.kakaoMap.selectedCode);
-  console.log(selectedCode);
-  const showKakaoMap = () => {
-    navigator.geolocation.getCurrentPosition((position) => {
-      let lat = position.coords.latitude; // 위도 y (0~90)
-      let lon = position.coords.longitude; // 경도 x (0~180)
-      const map = getKakaoMap(lat, lon);
-      searchByCategory(map);
-    });
-  };
 
   // 카카오 맵 객체를 생성하는 함수
-  const getKakaoMap = (lat, lon) => {
+  const getKakaoMap = ({ lat, lon }) => {
     const container = document.getElementById("kakao-map"); //지도를 담을 영역의 DOM 레퍼런스
     const options = {
       //지도를 생성할 때 필요한 기본 옵션
@@ -33,7 +25,7 @@ const KakaoMap = () => {
   // 카테고리를 기준으로 데이터를 호출하는 기능
   const searchByCategory = async (map) => {
     const rect = getCurrentMapArea(map).join(","); // 지도의 사각형 영역 구하기
-    const categoryGroupCode = "FD6";
+    const categoryGroupCode = selectedCode;
     const url = `${baseUrl}/search/category?category_group_code=${categoryGroupCode}&rect=${rect}`;
     const response = await fetch(url, {
       headers: {
@@ -45,8 +37,13 @@ const KakaoMap = () => {
   };
 
   useEffect(() => {
+    const showKakaoMap = async () => {
+      const location = await getCurrentLocaition();
+      const map = getKakaoMap(location);
+      searchByCategory(map);
+    };
     showKakaoMap();
-  }, []);
+  }, [selectedCode]);
   return <div id="kakao-map" />;
 };
 
