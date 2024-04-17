7bf75d7b0e0ddd4af0d9582e5b58ef7c7384be0c
JaeHyo0k <ahhpc2012@gmail.com>
2024-04-17 14:16:26 +0900

    TravelMapPage에 사이드바 생성

---
+++ src/pages/TravelMapPage/component/MapSideBar/MapSideBar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./MapSideBar.style.css";
+import { filterdCategoryCode } from "../../../../constants/categoryCode";
+import CategoryButton from "./component/CategoryButton";
+
+const MapSideBar = () => {
+  return (
+    <div id="map-sidebar">
+      <ul className="category-buttons">
+        {Object.entries(filterdCategoryCode).map(([code, name], idx) => (
+          <li key={idx}>
+            <CategoryButton categoryName={name} categoryCode={code} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default MapSideBar;
