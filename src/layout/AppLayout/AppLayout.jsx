9e2e21f30c7e93c34641aad9e4e3a46ad0a53456
sunae_shin <ko1607@naver.com>
2024-04-18 01:04:27 +0900

    navbar1

--- src/layout/AppLayout/AppLayout.jsx
+++ src/layout/AppLayout/AppLayout.jsx
@@ -1,17 +1,29 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import './AppLayout.style.css'
+import Button from "../../common/Button";
 
 const AppLayout = () => {
   return (
-    <div >
+    <div className="component">
       <div className="navbar">
-          <div>전국팔도</div>
-          <div>me1</div>
-          <div>me2</div>
-          <input placeholder="검색"></input>
-          <button>검색</button>
+          <div><img src="/img/logo.png" alt="logo" className="logo-image"/></div>
+          
+
+            <div className="meuntext">여행정보</div>
+            <div className="meuntext">여행지도</div>
+         
+          <div >
+            <input className="searchbox" placeholder="검색어를 입력하세요"></input>         
+            <Button>로그인</Button>
+          </div>
       </div>
+      <div className="imgBanner"></div>
+      <div className="textBanner">
+        두근두근 설레는<br/>봄꽃 여행지 추천<br/>
+        <Link>자세히보기</Link>
+      </div>
+
       <Outlet />
     </div>
   );
