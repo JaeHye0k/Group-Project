64b7e8eb66872bf5ddc77d3398975c0d7a9cfee1
JaeHyo0k <ahhpc2012@gmail.com>
2024-04-18 23:27:39 +0900

    스타일 수정

--- src/layout/AppLayout/AppLayout.jsx
+++ src/layout/AppLayout/AppLayout.jsx
@@ -1,14 +1,17 @@
-
 import React, { useState } from "react";
 import { Outlet, useNavigate, Link } from "react-router-dom";
-import './AppLayout.style.css'
+import "./AppLayout.style.css";
 import Button from "../../common/Button";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faTimes, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBars,
+  faTimes,
+  faMagnifyingGlass,
+} from "@fortawesome/free-solid-svg-icons";
 
 const AppLayout = () => {
   const navigate = useNavigate();
-  const [isMenuOpen, setIsMenuOpen] = useState(false); 
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -19,43 +22,62 @@ const AppLayout = () => {
     setIsMenuOpen(false); // 메뉴 선택 후 메뉴 닫기
   };
 
-  const onSearch =()=>{
-
-  }
+  const onSearch = () => {};
 
   return (
     <div className="app-layout">
       <nav className="app-navbar">
-        <FontAwesomeIcon className="menu-button" size="lg" icon={faBars} onClick={toggleMenu} />
+        <FontAwesomeIcon
+          className="menu-button"
+          size="lg"
+          icon={faBars}
+          onClick={toggleMenu}
+        />
         <div>
-          <img src="/img/logo.png" alt="logo" className="logo-image" onClick={() => navigateTo('/')}/>
+          <img
+            src="/img/logo.png"
+            alt="logo"
+            className="logo-image"
+            onClick={() => navigateTo("/")}
+          />
         </div>
         <div className="menu-texts">
-          <div className="menu-text" onClick={() => navigateTo('/')}>여행정보</div>
-          <div className="menu-text" onClick={() => navigateTo('/map')}>여행지도</div>
+          <div className="menu-text" onClick={() => navigateTo("/")}>
+            여행정보
+          </div>
+          <div className="menu-text" onClick={() => navigateTo("/map")}>
+            여행지도
+          </div>
         </div>
 
-        <div className={`menu ${isMenuOpen ? 'open' : ''}`}>
-          
+        <div className={`menu ${isMenuOpen ? "open" : ""}`}>
           {isMenuOpen && (
             <div>
               <div className="close-button" onClick={toggleMenu}>
-                <FontAwesomeIcon icon={faTimes}/>
+                <FontAwesomeIcon icon={faTimes} />
               </div>
-              <p className="mobile-menu-item" onClick={() => navigateTo('/')}>여행정보</p>
-              <p className="mobile-menu-item" onClick={() => navigateTo('/map')}>여행지도</p>
+              <p className="mobile-menu-item" onClick={() => navigateTo("/")}>
+                여행정보
+              </p>
+              <p
+                className="mobile-menu-item"
+                onClick={() => navigateTo("/map")}
+              >
+                여행지도
+              </p>
               <Button>로그인</Button>
             </div>
           )}
-
         </div>
 
         <div className="search_login">
           <div className="searchbox">
-          <input className='input' placeholder="검색어를 입력하세요"></input>
-          <FontAwesomeIcon icon={faMagnifyingGlass} onClick={onSearch}/>
-          </div>   
-          <Link to="/login" className="moblieLoginBtn">로그인</Link>
+            <input className="input" placeholder="검색어를 입력하세요"></input>
+            <FontAwesomeIcon icon={faMagnifyingGlass} onClick={onSearch} />
+          </div>
+          <button className="moblieLoginBtn">
+            <Link to="/login">로그인</Link>
+          </button>
         </div>
       </nav>
       <Outlet />
