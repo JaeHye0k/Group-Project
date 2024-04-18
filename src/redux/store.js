db7b194c5e15465e8aa83398b9caf552eab9d9f5
JaeHyo0k <ahhpc2012@gmail.com>
2024-04-18 20:44:58 +0900

    폴더 이동

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import kakaoMapSlice from "./reducers/kakaoMapSlice";
+import kakaoMapSlice from "./TravelMapStore/kakaoMapSlice";
 
 export const store = configureStore({
   reducer: {
