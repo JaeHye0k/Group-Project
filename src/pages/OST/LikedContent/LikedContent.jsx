7f93c8f88592329911d883034ee722c3f479d7ed
yangshoong <crinaeae83@gmail.com>
2024-04-19 01:17:10 +0900

    like content

--- src/pages/OST/LikedContent/LikedContent.jsx
+++ src/pages/OST/LikedContent/LikedContent.jsx
@@ -11,16 +11,17 @@ const LikedContent = () => {
         <h3 className="subtitle">여행관광정보(컨텐츠하트)</h3>
         <Row>
           {Array.from({ length: 10 }).map((_, index) => (
-              <div className="item">
-                <div className="item-box">
-                  <div className="heart"></div>
-                </div>
-                <div className="item-content">
-                  <h3 className="item-title">오래된 나무</h3>
-                  <p className="item-subtitle" />
+            <div className="item">
+              <div className="item-box">
+                <div className="heart"></div>
+              </div>
+              <div className="item-content">
+                <div className="item-title">오래된 나무</div>
+                <div className="item-subtitle">
                   충청남도 홍성군 홍북읍 상하천로 207
                 </div>
               </div>
+            </div>
           ))}
         </Row>
       </Container>
