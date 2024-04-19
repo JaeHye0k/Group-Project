ea5975cda315452a9dc308310cdcbf5c5699f28f
mjhn010 <mjhn010@naver.com>
2024-04-20 08:12:45 +0900

    attraction-complete

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import {store} from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 const queryClient = new QueryClient();
