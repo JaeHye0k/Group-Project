b96118c7496bc8d457f8ef473b3b303e7c1977f7
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2024-04-20 03:52:47 +0900

    지도 페이지에서만 body 태그 overflow:hidden으로 변경

--- src/pages/TravelMapPage/TravelMapPage.jsx
+++ src/pages/TravelMapPage/TravelMapPage.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import KakaoMap from "./component/KakaoMap/KakaoMap";
 import "./TravelMapPage.style.css";
 import MapSideBar from "./component/MapSideBar/MapSideBar";
+import { useLocation } from "react-router-dom";
 
 const TravelMapPage = () => {
+  const { pathname } = useLocation();
   return (
-    <div id="travel-map-page">
+    <div id="travel-map-page" className={pathname ? "overflow-hidden" : ""}>
       <KakaoMap />
       <MapSideBar />
     </div>
