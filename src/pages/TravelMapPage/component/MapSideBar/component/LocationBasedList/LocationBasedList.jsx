8c8a767be2e47112efd3667167c64b6550f4a98f
JaeHyo0k <ahhpc2012@gmail.com>
2024-04-19 15:59:08 +0900

    사이드 바 디자인 및 중심 클릭한 좌표 기준으로 근처 여행지 표시

--- src/pages/TravelMapPage/component/MapSideBar/component/LocationBasedList/LocationBasedList.jsx
+++ src/pages/TravelMapPage/component/MapSideBar/component/LocationBasedList/LocationBasedList.jsx
@@ -1,41 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { fetchLocationBasedList } from "../../../../../../utils/tourApi/tourApi";
 import { useQuery } from "@tanstack/react-query";
+import "./LocationBasedList.style.css";
+import LocationItem from "./LocationItem/LocationItem";
+
+// 렌더링 -> 함수 몸체 실행 -> useEffect
 
 const LocationBasedList = () => {
+  console.log("render!");
   const center = useSelector((state) => state.kakaoMap.center);
-  const { data, isLoading, isError, error, isFetched } = useQuery({
+  const { data, isLoading, isError, error, refetch } = useQuery({
     queryKey: ["location-based"],
     queryFn: () =>
       fetchLocationBasedList(center.lng, center.lat, center.radius),
     enabled: !!Object.keys(center).length, // center 값이 있을때만 fetch
   });
+
+  useEffect(() => {
+    if (!!Object.keys(center).length) {
+      refetch();
+    }
+  }, [center]);
+
+  const datas = data?.response?.body.items.item;
   if (isLoading) {
     return <div>Loading...</div>;
   }
   if (isError) {
     return <div>{error.message}</div>;
   }
-  if (isFetched) {
-    const datas = data.response.body.items.item;
-    console.log(datas);
-    return (
-      <ul>
-        {datas.map((item, idx) => {
-          // 썸네일이 있을 경우에만 표시
-          if (item.firstimage || item.firstimage2) {
-            return (
-              <li key={idx}>
-                <img src={item.firstimage || item.firstimage2} alt="썸네일" />
-                <div>{item.title}</div>
-              </li>
-            );
-          }
-        })}
-      </ul>
-    );
-  }
+
+  return (
+    <ul className="location-based-list">
+      {datas?.map((item, key) => {
+        // 썸네일이 있을 경우에만 표시
+        if (item.firstimage || item.firstimage2) {
+          return <LocationItem key={key} item={item} />;
+        }
+      })}
+    </ul>
+  );
 };
 
 export default LocationBasedList;
