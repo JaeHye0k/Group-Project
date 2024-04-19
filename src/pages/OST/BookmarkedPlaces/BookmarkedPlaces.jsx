54cf62ae1399bcb712be7eddd8c3fe7cd6f0e772
Yangshoong <crinaeae83@gmail.com>
2024-04-19 14:09:51 +0900

    컨텐츠하트, 지도북마크

--- src/pages/OST/BookmarkedPlaces/BookmarkedPlaces.jsx
+++ src/pages/OST/BookmarkedPlaces/BookmarkedPlaces.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from "react";
+import "./BookmarkedPlaces.style.css";
+import { Container, Row } from "react-bootstrap";
 
 const BookmarkedPlaces = () => {
   return (
-    <div>
-      
+    <div id="bookmarked-places" className="wrapper">
+      <Container className="container">
+        <div className="title-container">
+          <h1 className="title">즐겨찾기</h1>
+          <button className="map-search-btn">지도로보기</button>
+        </div>
+        <hr className="line" />
+        <h3 className="subtitle">지도정보(지도북마크)</h3>
+        <Row>
+          {Array.from({ length: 10 }).map((_, index) => (
+            <div className="item" key={index}>
+              <div className="item-box">
+                <div className="bookmark-img"></div>
+              </div>
+              <div className="item-content">
+                <div className="item-title">오래된 나무</div>
+                <div className="item-subtitle">
+                  충청남도 홍성군 홍북읍 상하천로 207
+                </div>
+              </div>
+            </div>
+          ))}
+        </Row>
+      </Container>
     </div>
-  )
-}
+  );
+};
 
-export default BookmarkedPlaces
+export default BookmarkedPlaces;
--- src/pages/OST/LikedContent/LikedContent.jsx
+++ src/pages/OST/LikedContent/LikedContent.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import "./LikedContent.style.css";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 
 const LikedContent = () => {
   return (
-    <div className="wrapper">
+    <div id="liked-content" className="wrapper">
       <Container className="container">
         <h1 className="title">즐겨찾기</h1>
         <hr className="line" />
         <h3 className="subtitle">여행관광정보(컨텐츠하트)</h3>
         <Row>
           {Array.from({ length: 10 }).map((_, index) => (
-            <div className="item">
+            <div className="item" key={index}>
               <div className="item-box">
-                <div className="heart"></div>
+                <div className="heart-img"></div>
               </div>
               <div className="item-content">
                 <div className="item-title">오래된 나무</div>
