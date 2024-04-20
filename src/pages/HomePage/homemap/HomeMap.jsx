82f9a520ca4321177f759e82d144d4dc9e065923
yangshoong <crinaeae83@gmail.com>
2024-04-20 21:30:36 +0900

    README

--- src/pages/HomePage/homemap/HomeMap.jsx
+++ src/pages/HomePage/homemap/HomeMap.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './HomeMap.style.css'
 // import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
+import KakaoMap from "../../TravelMapPage/component/KakaoMap/KakaoMap";
 
 const HomeMap = () => {
 
@@ -11,21 +12,30 @@ const HomeMap = () => {
     };
 
     return (
-        <div>
-            <br/>
-                <br/>
-                    <h3 className='mid-title'>이번엔 어디로 떠나볼까?</h3>
-            <div className='mapPosition'>                
-                        <div className='btnPosition'>
-                            <div className='mapbtn' onClick={() => navigateTo("/map")}>주변여행지</div>
-                            <div className='mapbtn' onClick={() => navigateTo("/map")}>음식점</div>
-                            <div className='mapbtn' onClick={() => navigateTo("/map")}>카페</div>
-                            <div className='mapbtn' onClick={() => navigateTo("/map")}>숙소</div>
-                        </div>
-                    <div className='homepageMap'>
-                        지도넣자</div>
+      <div>
+        <br />
+        <br />
+        <h3 className="mid-title">이번엔 어디로 떠나볼까?</h3>
+        <div className="mapPosition">
+          <div className="btnPosition">
+            <div className="mapbtn" onClick={() => navigateTo("/map")}>
+              주변여행지
             </div>
+            <div className="mapbtn" onClick={() => navigateTo("/map")}>
+              음식점
+            </div>
+            <div className="mapbtn" onClick={() => navigateTo("/map")}>
+              카페
+            </div>
+            <div className="mapbtn" onClick={() => navigateTo("/map")}>
+              숙소
+            </div>
+          </div>
+          <div className="homepageMap">
+            <KakaoMap />
+          </div>
         </div>
+      </div>
     );
 };
 
